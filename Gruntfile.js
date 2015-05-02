eb9a6db167d5d81aa3b9d47cfb32e0cc160319f4
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-03 01:23:34 +0200

    use `path-exists` module as `fs.exists` is being deprecated
    https://github.com/sindresorhus/path-exists

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,14 +9,9 @@ module.exports = function (grunt) {
 					dest: 'tmp'
 				}
 			}
-		},
-		clean: {
-			test: ['tmp/**']
 		}
 	});
 
 	grunt.loadTasks('tasks');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-
-	grunt.registerTask('default', ['clean', 'pageres']);
+	grunt.registerTask('default', ['pageres']);
 };
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 'use strict';
 var test = require('ava');
-var fs = require('fs');
 var del = require('del');
+var pathExists = require('path-exists');
 
 test(function (t) {
-	t.assert(fs.existsSync('tmp/yeoman.io-1000x1000.png'));
-	t.assert(fs.existsSync('tmp/yeoman.io-100x100.png'));
+	t.assert(pathExists.sync('tmp/yeoman.io-1000x1000.png'));
+	t.assert(pathExists.sync('tmp/yeoman.io-100x100.png'));
 	del.sync('tmp');
 	t.end();
 });
