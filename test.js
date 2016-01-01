7841afd902672b0a282b280d735f9214885fdc8c
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 01:51:06 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -4,11 +4,9 @@ import pathExists from 'path-exists';
 test('pageres', t => {
 	t.true(pathExists.sync('tmp/yeoman.io-1000x1000.png'));
 	t.true(pathExists.sync('tmp/yeoman.io-100x100.png'));
-	t.end();
 });
 
 test('multiple URLs', t => {
 	t.true(pathExists.sync('tmp/yeoman.io-1000x1000.png'));
 	t.true(pathExists.sync('tmp/google.com-1000x1000.png'));
-	t.end();
 });
